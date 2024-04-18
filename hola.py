e39dcb55ea3abfdb3c2e9ea7ef8e2a8bea57ba54
DianaGMD <a01368899@tec.mx>
2024-04-17 19:33:05 -0600

    histograma

--- hola.py
+++ hola.py
@@ -1,6 +1,7 @@
 # importamos la biblioteca streamlit
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
 # creamos el titulo de la App
 st.title("Titanic App")
 st.header("Titanic Graphs -App")
@@ -10,4 +11,9 @@ st.write("Graficas del dataset Titanic")
 titanic_link = 'Titanic.csv'
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
+fig, ax = plt.subplots()
+ax.hist(titanic_data["Fare"])
+st.header("Histograma del Titanic")
+st.pyplot(fig)
 
