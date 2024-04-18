bb07dd04697a8af7acac8e890139faf741d6eda5
DianaGMD <a01368899@tec.mx>
2024-04-17 19:19:50 -0600

    nombre

--- hola.py
+++ hola.py
@@ -4,6 +4,7 @@ import pandas as pd
 # creamos el titulo de la App
 st.title("Titanic App")
 st.header("Titanic Graphs -App")
+st.header("Diana Gonzalez Munguia")
 st.write("Graficas del dataset Titanic")
 
 titanic_link = 'Titanic.csv'
